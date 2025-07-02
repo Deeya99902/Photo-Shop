1e38cad3ab2dbe92d9844757b340cd93a209dcca
Deeya99902 <deeyaphuyal13@gmail.com>
2025-07-02 16:56:53 +0545

    completed views and urls for our project

--- photoshop/gallery/views.py
+++ photoshop/gallery/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
+def product_list(request):
+    Products = Product.objects.all()
+    return render(request,'index.html',{'products':Products})
+
+
+def home(request):
+    return HttpResponse('Hello how are you')
+
--- photoshop/photoshop/settings.py
+++ photoshop/photoshop/settings.py
@@ -116,8 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFIELDS_DIRS = [BASE_DIR /"static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = "/media/"
+MEDIAL_ROOT = BASE_DIR,"/media/"
